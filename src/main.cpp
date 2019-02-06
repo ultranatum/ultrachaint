a5de32e2d7da7ab8cd4846dfe4f731c892a19a9e
Ultradev <ultranatum@web.de>
2019-02-06 14:18:12 +0300

    Update version

--- src/main.cpp
+++ src/main.cpp
@@ -6178,7 +6178,7 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
 //       it was the one which was commented out
 int ActiveProtocol()
 {
-    // SPORK_14 is used for 70913 (v3.1.0+)
+    // SPORK_14 is used for 70915
     if (IsSporkActive(SPORK_14_NEW_PROTOCOL_ENFORCEMENT))
             return MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT;
 
